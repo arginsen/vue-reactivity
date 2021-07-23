d8cc9cdada096c6b8e96f45a49fcd7a819d2d7b1
arginsen <arginsen19@163.com>
2021-07-23 21:50:36 +0800

    @finish to edit

--- vue/compile/index.js
+++ vue/compile/index.js
@@ -2,168 +2,168 @@ import Watcher from "../reactivity/watcher.js"
 
 // 专门负责解析模板内容
 export default class Compile {
-  /**
-   * @param {} 传递的选择器
-   * @param {} Vue实例
-   */
-  constructor (el, vm) {
-      // 如果用户直接给 el 赋值了一个 DOM 对象，这样也可以
-      this.el = typeof el === 'string' ? document.querySelector(el) : el
-      this.vm = vm
+	/**
+	 * @param {} 传递的选择器
+	 * @param {} Vue实例
+	 */
+	constructor(el, vm) {
+		// 如果用户直接给 el 赋值了一个 DOM 对象，这样也可以
+		this.el = typeof el === 'string' ? document.querySelector(el) : el
+		this.vm = vm
 
-      // 编译模板内容（把插值表达式，指令都替换）
-      if (this.el) {
-          
-          // 1.把el中所有的节点放到 fragment（文档碎片）
-          let fragment = this.node2fragment(this.el)
+		// 编译模板内容（把插值表达式，指令都替换）
+		if (this.el) {
 
-          // 2.编译 fragment
-          this.compile(fragment)
+			// 1.把el中所有的节点放到 fragment（文档碎片）
+			let fragment = this.node2fragment(this.el)
 
-          // 3.把 fragment 的内容一次放到 DOM 中
-          this.el.appendChild(fragment)
-      }
-  }
+			// 2.编译 fragment
+			this.compile(fragment)
 
-  /** 核心方法 */
-  
-  // 把我们的节点，转为 代码片段
-  node2fragment(el) {
+			// 3.把 fragment 的内容一次放到 DOM 中
+			this.el.appendChild(fragment)
+		}
+	}
 
-      let fragment = document.createDocumentFragment()
+	/** 核心方法 */
 
-      // 把el中所有的子节点 挨个添加到 文档碎片中
-      let childNodes = el.childNodes // 类数组
-      this.toArray(childNodes).forEach(item => {
-          fragment.appendChild(item) // 把el中所有的子节点 挨个添加到 文档碎片中
-      })
-      return fragment
+	// 把我们的节点，转为 代码片段
+	node2fragment(el) {
 
-  }
+		let fragment = document.createDocumentFragment()
 
-  /**
-   * 编译文档碎片
-   * @param {*} fragment 
-   */
-  compile(fragment) {
-      let childNodes = fragment.childNodes // 拿到所有的子节点
-      this.toArray(childNodes).forEach(node => {
-          
-          // 如果是 元素（标签），需要解析指令
-          if (this.isElementNode(node)) {
-              this.compileElement(node) // 解析元素（标签）节点
-          }
+		// 把el中所有的子节点 挨个添加到 文档碎片中
+		let childNodes = el.childNodes // 类数组
+		Utils.toArray(childNodes).forEach(item => {
+			fragment.appendChild(item) // 把el中所有的子节点 挨个添加到 文档碎片中
+		})
+		return fragment
 
-          // 如果是文本节点，需要解析 插值表达式
-          if (this.isTextNode(node)) {
-              this.compileText(node) // 解析文本节点
-          }
+	}
 
-          // 如果当前节点还有子节点的时候，需要递归的判断
-          if (node.childNodes && node.childNodes.length > 0) {
-              this.compile(node)
-          }
+	/**
+	 * 编译文档碎片
+	 * @param {*} fragment 
+	 */
+	compile(fragment) {
+		let childNodes = fragment.childNodes // 拿到所有的子节点
+		Utils.toArray(childNodes).forEach(node => {
 
-      })
-  }
+			// 如果是 元素（标签），需要解析指令
+			if (Utils.isElementNode(node)) {
+				Utils.compileElement(node) // 解析元素（标签）节点
+			}
 
-  // 解析元素（标签）节点
-  compileElement (node) {
-      // 思路：所谓指令，就是 HTML 的一个 v 开头的特殊属性
-      // 1.获取当前节点下所有的属性
-      let attributes = node.attributes // 类数组
-      this.toArray(attributes).forEach(attr => {
-          
-          let attrName = attr.name
+			// 如果是文本节点，需要解析 插值表达式
+			if (Utils.isTextNode(node)) {
+				this.compileText(node) // 解析文本节点
+			}
 
-          // 2.解析Vue的指令（ v- 开头的）
-          if (this.isDirective(attr.nodeName)) {
-              let type = attrName.slice(2)
-              let attrValue = attr.value
+			// 如果当前节点还有子节点的时候，需要递归的判断
+			if (node.childNodes && node.childNodes.length > 0) {
+				this.compile(node)
+			}
 
-              if (this.isEventDirective(type)) {
-                  // 解析 v-on 指令
-                  // 给当前元素注册事件
-                  let eventType = type.split(':')[1] // 事件类型
-                  node.addEventListener(eventType, this.vm.$methods[attrValue].bind(this.vm))    
-              } else {
-                  ComileUtil[type](node, this.vm, attrValue)
-              }
-          }
+		})
+	}
 
-      })
-  }
+	// 解析元素（标签）节点
+	compileElement(node) {
+		// 思路：所谓指令，就是 HTML 的一个 v 开头的特殊属性
+		// 1.获取当前节点下所有的属性
+		let attributes = node.attributes // 类数组
+		Utils.toArray(attributes).forEach(attr => {
 
-  // 解析文本节点
-  compileText(node) {
+			let attrName = attr.name
 
-      let txt = node.textContent
-      let reg = /\{\{(.+)\}\}/
-      if (reg.test(txt)) {
-          let expr = RegExp.$1 // $1 拿到第一个分组
-          node.textContent = txt.replace(reg, this.vm.$data[expr])
+			// 2.解析Vue的指令（ v- 开头的）
+			if (Utils.isDirective(attrName)) {
+				let attrValue = attr.value
 
-          // 新增一个观察者，传入回调，通过回调函数直接更新
-          new Watcher(this.vm, expr, (newValue, oldValue) => {
-              node.textContent = txt.replace(reg, newValue)
-          })
-      }
+				if (Utils.isEventDirective(attrName)) {
+					// 解析 v-on 指令   给当前元素注册事件
+					let eventType = attrName.startsWith('v-on') ? attrName.split(':')[1] : attrName.split('@')[1] // 事件类型
+					node.addEventListener(eventType, this.vm.$methods[attrValue].bind(this.vm))
+				} else {
+					DirectivesUtils[type](node, this.vm, attrValue)
+				}
+			}
+		})
+	}
 
-  }
+	// 解析文本节点
+	compileText(node) {
+		let txt = node.textContent
+		let reg = /\{\{(.+)\}\}/
+		if (reg.test(txt)) {
+			let expr = RegExp.$1 // $1 拿到第一个分组
+			node.textContent = txt.replace(reg, this.vm.$data[expr])
 
-
-
-  /** 工具方法 */
-  // 类数组 ---> 数组
-  toArray (likeArray) {
-      return [].slice.call(likeArray)
-  }
-  // 是否是元素节点 | 1：元素节点 | 3：文本节点
-  isElementNode (node) {
-      return node.nodeType === 1
-  }
-  isTextNode (node) {
-      return node.nodeType === 3
-  }
-  // 是否是指令
-  isDirective (attrName) {
-      return attrName.startsWith('v-') // ES6 字符串方法，是否以某个开头
-  }
-  // 是否是一个事件指令 ：v-on:click 这样的
-  isEventDirective (attrName) {
-      return attrName.split(':')[0] === 'on'
-  }
+			// 新增一个观察者，传入回调，通过回调函数直接更新
+			new Watcher(this.vm, expr, (newValue, oldValue) => {
+				node.textContent = txt.replace(reg, newValue)
+			})
+		}
+	}
 }
 
-let ComileUtil = {
+/* 
+ * 工具方法 
+ */
+const Utils = {
+	// 类数组 ---> 数组
+	toArray(likeArray) {
+		return [].slice.call(likeArray)
+	},
+	// 是否是元素节点 | 1：元素节点 | 3：文本节点
+	isElementNode(node) {
+		return node.nodeType === 1
+	},
+	isTextNode(node) {
+		return node.nodeType === 3
+	},
+	// 是否是指令 - ES6 字符串方法，是否以某个开头
+	isDirective(attrName) {
+		return attrName.startsWith('v-') || attrName.startsWith(':') || attrName.startsWith('@')
+	},
+	// 是否是一个事件指令 ：v-on:click 这样的
+	isEventDirective(attrName) {
+		return ttrName.startsWith('v-on') || attrName.startsWith('@')
+	}
+}
 
-  // 处理 v-text 指令
-  text (node, vm, attrValue) {
-      node.textContent = vm.$data[attrValue]
-      new Watcher(vm, attrValue, (newValue, oldValue) => {
-          node.textContent = newValue
-      })
-  },
+/* 
+ * 指令解析方法 
+ */
+const DirectivesUtils = {
 
-  // 解析 v-html 指令
-  html (node, vm, attrValue) {
-      node.innerHTML = vm.$data[attrValue]
-      new Watcher(vm, attrValue, (newValue, oldValue) => {
-          node.innerHTML = newValue
-      })
-  },
+	// 处理 v-text 指令
+	text(node, vm, attrValue) {
+		node.textContent = vm.$data[attrValue]
+		new Watcher(vm, attrValue, (newValue, oldValue) => {
+			node.textContent = newValue
+		})
+	},
 
-  // 解析 v-model 指令
-  model (node, vm, attrValue) {
-      node.value = vm.$data[attrValue]
+	// 解析 v-html 指令
+	html(node, vm, attrValue) {
+		node.innerHTML = vm.$data[attrValue]
+		new Watcher(vm, attrValue, (newValue, oldValue) => {
+			node.innerHTML = newValue
+		})
+	},
 
-      // 注册事件
-      node.addEventListener('input', e => {
-        vm.$data[attrValue] = event.target.value
-      })
+	// 解析 v-model 指令
+	model(node, vm, attrValue) {
+		node.value = vm.$data[attrValue]
 
-      new Watcher(vm, attrValue, (newValue, oldValue) => {
-          node.value = newValue
-      })
-  }
+		// 注册事件
+		node.addEventListener('input', e => {
+			vm.$data[attrValue] = event.target.value
+		})
+
+		new Watcher(vm, attrValue, (newValue, oldValue) => {
+			node.value = newValue
+		})
+	}
 }
